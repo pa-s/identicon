5e2f3867ce1fb890b8a8e0d45d2429dfd780a9db
Sarah Pimentel <sarah.pimentel@gmail.com>
2017-03-23 14:31:19 +0100

    create grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,9 +3,20 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
-  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do    
+  def build_grid(%Identicon.Image{hex: hex_list} = image) do
+    hex_list
+    |> Enum.chunk(3)
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
